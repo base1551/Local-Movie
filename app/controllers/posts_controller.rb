7f9263990b6ac5c65ce2d72ce6d32498a6b1dcee
base1551 <baseball.1.kyoto@gmail.com>
2019-12-22 17:23:12 +0900

    投稿画面作成

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,36 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: %i(show destroy)
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    if @post.save
+      redirect_to root_path, notice: '投稿しました'
+    else
+      render :new
+    end
+  end
+
+  def index
+    @posts = Post.all.order('created_at DESC')
+  end
+
+  def show
+  end
+
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    redirect_to root_path, notice: '削除しました' if @post.destroy
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :body, :video)
+    end
+
+    def set_post
+      @post = Post.find_by(id: params[:id])
+    end
+end
